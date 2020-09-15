2080055a123ce27ff215d6d153ba535a8bc8f375
Ronnie Song <ronniesong0809@gmail.com>
2020-09-15 00:47:37 -0700

    add relational-data-access

--- consuming-rest/src/main/java/com/ronsong/consumingrest/Quote.java
+++ consuming-rest/src/main/java/com/ronsong/consumingrest/Quote.java
@@ -1,2 +1,32 @@
-package com.ronsong.consumingrest;public class Quote {
+package com.ronsong.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+    private String type;
+    private Value value;
+
+    public Quote() {}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "quote{" + "type='" + type + '\'' + ", value=" + value + '}';
+    }
 }
---
+++ relational-data-access/src/main/java/com/ronsong/relationaldataaccess/RelationalDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.ronsong.relationaldataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDataAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationalDataAccessApplication.class, args);
+    }
+
+}
