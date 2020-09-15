6b97d8bd52076c1ea16b38164a4e1537894e8804
Ronnie Song <ronniesong0809@gmail.com>
2020-09-15 02:51:09 -0700

    WIP

--- uploading-files/src/test/java/com/ronsong/uploadingfiles/UploadingFilesApplicationTests.java
+++ uploading-files/src/test/java/com/ronsong/uploadingfiles/UploadingFilesApplicationTests.java
@@ -1,13 +1,37 @@
 package com.ronsong.uploadingfiles;
 
+import com.ronsong.uploadingfiles.storage.StorageService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UploadingFilesApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private StorageService storageService;
+
     @Test
-    void contextLoads() {
+    public void shouldListAllFiles() throws Exception {
+        given(this.storageService.loadAll()).willReturn(Stream.of(Paths.get("test.txt1"), Paths.get("test2.txt")));
+        this.mvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(model().attribute("file", Matchers.contains("http://localhost/files/test1.txt", "http://localhost/files/test2.txt")));
     }
 
 }
