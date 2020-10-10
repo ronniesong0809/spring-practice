1f924101dc44b54fb3a3f444b5bf0b8a0f7ef61b
Ronnie Song <ronniesong0809@gmail.com>
2020-10-10 04:34:55 -0700

    neo4j done

---
+++ accessing-data-neo4j/src/main/java/com/ronsong/accessingdataneo4j/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.ronsong.accessingdataneo4j;
+
+import java.util.List;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+public interface PersonRepository extends Neo4jRepository<Person, Long> {
+    Person findByName(String name);
+    List<Person> findByTeammatesName(String name);
+}
