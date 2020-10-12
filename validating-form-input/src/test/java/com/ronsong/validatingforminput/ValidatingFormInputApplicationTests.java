7e734e45dcb37792f64ed435bea2a3e4afe7fd8a
Ronnie Song <ronniesong0809@gmail.com>
2020-10-11 17:06:08 -0700

    validating form input

---
+++ validating-form-input/src/test/java/com/ronsong/validatingforminput/ValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronsong.validatingforminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
