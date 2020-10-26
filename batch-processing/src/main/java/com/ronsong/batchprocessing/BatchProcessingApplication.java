cf4a2159c3a8c791e808a93c5e1dfdbed09298df
Ronnie Song <ronniesong0809@gmail.com>
2020-10-26 13:24:12 -0700

    a batch service

---
+++ batch-processing/src/main/java/com/ronsong/batchprocessing/BatchProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.ronsong.batchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingApplication {
+
+    public static void main(String[] args) throws Exception {
+        // SpringApplication.run(BatchProcessingApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(BatchProcessingApplication.class, args)));
+    }
+
+}
