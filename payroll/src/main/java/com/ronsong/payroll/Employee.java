277b5c699407741fb731780bea7b9864134e7584
Ronnie Song <ronniesong0809@gmail.com>
2020-10-30 15:53:47 -0700

    get employee class ready for JPA

---
+++ payroll/src/main/java/com/ronsong/payroll/Employee.java
@@ -0,0 +1,61 @@
+package com.ronsong.payroll;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+    private @Id @GeneratedValue long id;
+    private String name;
+    private String role;
+
+    Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Employee)) {
+            return false;
+        }
+        Employee employee = (Employee) o;
+        return Objects.equals(this.id, employee.id)
+                && Objects.equals(this.name, employee.name)
+                && Objects.equals(this.role, employee.role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + this.id + ", name=" + this.name + ", role" + this.role + "}";
+    }
+}
