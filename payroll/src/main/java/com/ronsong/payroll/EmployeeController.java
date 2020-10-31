58ddbbbc9ae708c352313d7ce631e948bd0b0c26
Ronnie Song <ronniesong0809@gmail.com>
2020-10-31 03:24:56 -0700

    make it more RESTful

--- payroll/src/main/java/com/ronsong/payroll/EmployeeController.java
+++ payroll/src/main/java/com/ronsong/payroll/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.ronsong.payroll;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -24,8 +26,11 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
